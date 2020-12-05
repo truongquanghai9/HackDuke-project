f077ab151a21c6d3dfff256cd06e57a3f06e412e
jeff-tan-2020 <jeta7102@gmail.com>
2020-12-05 15:00:02 -0500

    Topic

--- src/Topic.java
+++ src/Topic.java
@@ -1,6 +1,20 @@
-public abstract class Topic {
 
-    public abstract Topic getTopic();
+import java.util.*;
+public class Topic {
+    private String[] topics = {"biology", "chemistry", "physics", "math", "computer science"};
 
+    private String myTopic;
+
+    public Topic(String topic){
+        if(!Arrays.asList(topics).contains(topic)){
+            myTopic = "other";
+        }
+        else{
+            myTopic = topic;
+        }
+    }
+    public String getTopic(){
+        return myTopic;
+    }
 
 }
