eb1ac5ae4edb7a24f0d78e04e3efbdb6dad2b88d
jeff-tan-2020 <jeta7102@gmail.com>
2020-12-05 16:35:05 -0500

    Search

---
+++ src/Search.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Search {
+    private static final int MAX_SIZE = 10;
+    private static final int MAX_PREFIX = 5;
+    public  List<Article> Search(String search, List<Article> articles){
+        ArrayList<Article> ret = new ArrayList<>();
+        Comparator<String> stringLengthComparator = Comparator.comparingInt(String::length);
+        PriorityQueue<String> keywords = new PriorityQueue<>(stringLengthComparator);
+
+        for(String word : search.split(" ")){
+            if(keywords.size()< MAX_PREFIX){
+                keywords.add(word);
+            }
+            else{
+                keywords.add(word);
+                keywords.remove();
+            }
+        }
+
+        TreeMap<Integer, ArrayList<Article>> compare = new TreeMap<>(Collections.reverseOrder());
+
+        for(Article a : articles){
+            ArrayList<String> words = new ArrayList<>(Arrays.asList(a.getTitle().split(" ")));
+            words.retainAll(keywords);
+            int matches = words.size();
+            compare.putIfAbsent(matches, new ArrayList<Article>());
+            compare.get(matches).add(a);
+        }
+
+        for(int i : compare.descendingKeySet()){
+            for(Article art : compare.get(i)){
+                ret.add(art);
+                if(ret.size() == MAX_SIZE){
+                    return ret;
+                }
+            }
+        }
+        return ret;
+
+    }
+}
